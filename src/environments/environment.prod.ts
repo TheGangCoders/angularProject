3f3f8e13ea2793ba44ea8ac7688e5afe19591241
alphacodergabo <gabo.9218@gmail.com>
2022-06-19 21:38:43 -0500

    cambio en cadenas de conexion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,9 @@ export const environment = {
   production: true,
   api: {
     WS_Core: {
-      url: 'http://localhost:5000/api',
+      //url: 'http://localhost:5000/api',
       // url: 'http://webapi-dev.us-west-2.elasticbeanstalk.com/api',
+      url: 'https://tiolalo.azurewebsites.net/api',
     }
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,9 @@ export const environment = {
   production: false,
   api: {
     WS_Core: {
-      url: 'http://localhost:5000/api',
+      //url: 'http://localhost:5000/api',
       // url: 'http://webapi-dev.us-west-2.elasticbeanstalk.com/api',
+      url: 'https://tiolalo.azurewebsites.net/api',
     }
   }
 };
